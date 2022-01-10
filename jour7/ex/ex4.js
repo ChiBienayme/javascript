07ba82df93614b39c98b9b646be4a81af0e50ce2
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-10 15:41:21 +0100

    24th commit

--- jour7/ex/ex4.js
+++ jour7/ex/ex4.js
@@ -7,3 +7,13 @@ const numbers = array.filter(function numbersOnly(value) {
         }
     });
 console.log(numbers);
+
+
+// V2
+const array = [1,"toto",34,"javascript",8];
+const numbers = array.filter(function(num){
+    return num.toString.length;
+}
+);
+
+console.log(numbers);
---
+++ jour7/ex/ex6.js
@@ -0,0 +1,36 @@
+
+// 06 - Cakes
+const cake = [
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
+]
+
+const soldOut = cake.filter(function(value) {
+    if (value.flavor === 'chocolate') {
+        return value.status = "sold out !" ;
+    }
+});
+console.log(soldOut);
