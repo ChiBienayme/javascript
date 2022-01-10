b5f24ecbd08eb5aba231dc4ca1cf07d548208927
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-10 14:01:49 +0100

    24th commit

---
+++ jour7/ex/ex2.js
@@ -0,0 +1,8 @@
+//02 - Map Double
+const array= [1, 2, 3, 4, 5];
+
+const doubles = array.map(function(num) {
+    return num * 2;
+});
+console.log(array, doubles);
+//[ 1, 2, 3, 4, 5 ] [ 2, 4, 6, 8, 10 ]
---
+++ jour7/ex/ex3.js
@@ -0,0 +1,17 @@
+// 03 - Map Names
+const longNames = [
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+];
+const shortNames = longNames.map(function(name) {
+    return `{name: ${name.firstName} ${name.lastName}}`;
+});
+
+console.log(shortNames);
+
