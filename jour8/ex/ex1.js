634e474b82a938944a33de132a5871fafad09f45
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-11 14:55:45 +0100

    25th commit

---
+++ jour8/ex/ex1.js
@@ -0,0 +1,21 @@
+// 01 - Countries
+
+const axios = require("axios");
+
+const countriesNames = [];
+
+// axios.get("https://restcountries.com/rest/v3.1/all")
+// .then(function(res) {
+//         countriesNames = res.data
+//             .map(function(getCountries) {
+//                 countriesNames = getCountries.name.common
+//                  return getCountries += countriesNames; 
+//             });
+        
+// })
+
+
+
+console.log(getCountries);
+
+
